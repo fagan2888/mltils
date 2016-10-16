c3a3a2bf01729c0d64a0fc3929e200ff9cc9f292
rladeira <rwladeira@gmail.com>
2016-10-16 12:18:13 -0200

    Add unit tests for CountEncoder

--- tests/mltils/test_encoders.py
+++ tests/mltils/test_encoders.py
@@ -2,13 +2,67 @@
 import numpy as np
 import pandas as pd
 
-from mltils.encoders import DummyEncoder
+from mltils.encoders import CountEncoder, DummyEncoder
 from mltils.utils import nan_equal
 
 
+def test_count_encoder_1():
+    cenc = CountEncoder()
+    assert cenc is not None
+
+
+def test_count_encoder_2():
+    df = pd.DataFrame(
+        {'A': ['a', 'a', 'a'],
+         'B': ['c', 'c', 'd'],
+         'C': [1, 2, 3]})
+    encoded = CountEncoder().fit_transform(df)
+    expected = pd.DataFrame(
+        {'A_count': [3., 3., 3.],
+         'B_count': [2., 2., 1.],
+         'C_count': [1., 1., 1.]})
+    assert expected.equals(encoded)
+
+
+def test_count_encoder_3():
+    df = pd.DataFrame(
+        {'A': ['a', np.nan, np.nan],
+         'B': ['c', 'c', 'd'],
+         'C': [1, 2, np.nan]})
+    encoded = CountEncoder().fit_transform(df)
+    expected = pd.DataFrame(
+        {'A_count': [1., 2., 2.],
+         'B_count': [2., 2., 1.],
+         'C_count': [1., 1., 1.]})
+    assert expected.equals(encoded)
+
+
+def test_count_encoder_4():
+    df = pd.DataFrame(
+        {'A': ['a', np.nan, np.nan],
+         'B': ['c', 'c', 'd'],
+         'C': [1, 2, np.nan]})
+    df_before = df.copy()
+    _ = CountEncoder().fit_transform(df)
+    assert df_before.equals(df)
+
+
+def test_count_encoder_5():
+    df = pd.DataFrame(
+        {'A': ['a', np.nan, np.nan],
+         'B': ['c', 'c', 'd'],
+         'C': [1, 2, np.nan]})
+    encoded = CountEncoder(variables=['A', 'B']).fit_transform(df)
+    expected = pd.DataFrame(
+        {'A_count': [1., 2., 2.],
+         'B_count': [2., 2., 1.]})
+    assert expected.equals(encoded)
+
+
+
 def test_dummy_encoder_1():
-    encoder = DummyEncoder()
-    assert encoder is not None
+    denc = DummyEncoder()
+    assert denc is not None
 
 
 def test_dummy_encoder_2():
