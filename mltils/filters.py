b4f8e8d38636a52d3d4b199fdc670ff93eca33f6
rladeira <rwladeira@gmail.com>
2016-10-18 07:35:13 -0200

    Add prototype for filters module.

---
+++ mltils/filters.py
@@ -0,0 +1,13 @@
+# pylint: disable=missing-docstring, invalid-name, import-error
+
+
+class VarianceFilter(object):
+    pass
+
+
+class SimilarityFilter(object):
+    pass
+
+
+class CorrelationFilter(object):
+    pass
